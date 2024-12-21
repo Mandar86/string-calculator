35afb9ed23e23105a9c5562777946cb89e044bfe
Mandar <mandarjvategavkar45@gmail.com>
2024-12-21 12:35:58 +0530

    test: ensure handling of multiple numbers separated by commas

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -11,3 +11,7 @@ test("should return the number when only one number is passed", () => {
 test("should return the sum when two number separated by comma are passed", () => {
   expect(stringCalculator("1,2")).toBe(3);
 });
+
+test("should handle multiple numbers separated by commas", () => {
+  expect(stringCalculator("4,5,6,7,8")).toBe(4 + 5 + 6 + 7 + 8);
+});
