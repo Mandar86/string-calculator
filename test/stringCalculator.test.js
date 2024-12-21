9ff1d10357885db3ba19cfbdf3ea092b6255d08d
Mandar <mandarjvategavkar45@gmail.com>
2024-12-21 12:13:07 +0530

    test: add test for single number

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -3,3 +3,7 @@ const stringCalculator = require("../src/stringCalculator");
 test("should return 0 for empty string", () => {
   expect(stringCalculator("")).toBe(0);
 });
+
+test("should return the number when only one number is passed", () => {
+  expect(stringCalculator("1")).toBe(1);
+});
