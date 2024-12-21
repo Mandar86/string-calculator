99a52f7731780c0167a73a296a7d9349e7946b07
Mandar <mandarjvategavkar45@gmail.com>
2024-12-21 16:13:14 +0530

    refactor: clean up string calculator implementation

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,14 +1,11 @@
 function stringCalculator(inputString) {
-  if (!inputString) {
-    return 0;
-  }
+  if (!inputString) return 0;
 
-  let delimiter = /,|\n/; // regular expression to handle two delimiters either comma(,) or newline(\n)
+  let delimiter = /,|\n/;
 
   if (inputString.startsWith("//")) {
-    // Input Format:- //[delimiter]\n[numbers…]
     const partitions = inputString.split("\n");
-    delimiter = new RegExp(partitions[0].slice(2)); // Regular expression of custom delimiter
+    delimiter = new RegExp(partitions[0].slice(2));
     inputString = partitions[1];
   }
   const numbersArray = inputString.split(delimiter).map(Number);
