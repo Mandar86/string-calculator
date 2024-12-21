2ddc713b9cb474a9a0808290b5ab2405e813efeb
Mandar <mandarjvategavkar45@gmail.com>
2024-12-21 13:07:24 +0530

    feat: support custom delimiters in input

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,7 +2,15 @@ function stringCalculator(inputString) {
   if (!inputString) {
     return 0;
   }
-  const delimiter = /,|\n/; // regular expression to handle two delimiters either comma(,) or newline(\n)
+
+  let delimiter = /,|\n/; // regular expression to handle two delimiters either comma(,) or newline(\n)
+
+  if (inputString.startsWith("//")) {
+    // Input Format:- //[delimiter]\n[numbers…]
+    const partitions = inputString.split("\n");
+    delimiter = new RegExp(partitions[0].slice(2)); // Regular expression of custom delimiter
+    inputString = partitions[1];
+  }
   const numbersArray = inputString.split(delimiter).map(Number);
 
   return numbersArray.reduce((sum, curr) => sum + curr);
