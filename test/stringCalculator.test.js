0e40e27c6dc2ffc27cfac734ff25a126e0cfa670
Mandar <mandarjvategavkar45@gmail.com>
2024-12-21 12:25:49 +0530

    test: add test for two numbers separated by a comma

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -7,3 +7,7 @@ test("should return 0 for empty string", () => {
 test("should return the number when only one number is passed", () => {
   expect(stringCalculator("1")).toBe(1);
 });
+
+test("should return the sum when two number separated by comma are passed", () => {
+  expect(stringCalculator("1,2")).toBe(3);
+});
