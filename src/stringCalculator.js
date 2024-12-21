db5bd6a1e109a908d276cf86a3388d7ba8504174
Mandar <mandarjvategavkar45@gmail.com>
2024-12-21 12:46:48 +0530

    feat: support newline as a delimiter

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,7 +2,8 @@ function stringCalculator(inputString) {
   if (!inputString) {
     return 0;
   }
-  const numbersArray = inputString.split(",").map(Number);
+  const delimiter = /,|\n/; // regular expression to handle two delimiters either comma(,) or newline(\n)
+  const numbersArray = inputString.split(delimiter).map(Number);
 
   return numbersArray.reduce((sum, curr) => sum + curr);
 }
